4f39fc692c5033baff5ef902d419b6bcdb330fa8
microjeco <microjeco@gmail.com>
2021-05-19 16:58:17 +0200

    inserito nel carousel le immagini della cartella img_home, gestite con il .map nel carousel.component.ts

--- src/app/home/carousel/carousel.component.ts
+++ src/app/home/carousel/carousel.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+var immagine1 = "../assets/img_home/logoburger.png";
+var immagine2 = "../assets/img_home/sulu-burger1.jpg";
+var myUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/1200px-A_small_cup_of_coffee.JPG';
+
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
@@ -12,4 +16,7 @@ export class CarouselComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  
+  images = ['sulu-burger1.jpg', 'logoburger.png', 'myUrl'].map((n) => `../assets/img_home/${n}`);
+  
 }
