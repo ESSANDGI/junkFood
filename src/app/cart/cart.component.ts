b9eacfcdd32e17ed87bd2f7f617813925af98b31
MatthewOvens <fornaramatteo@gmail.com>
2021-05-27 14:53:36 +0200

    Prime funzionalità carrello

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,31 +1,5 @@
+import { ShoppingCartService } from './../services/shopping-cart.service';
 import { Component, OnInit } from '@angular/core';
-
-export class ShoppingCart {
-
-  private productsArray : number[];
-  
-  constructor(products : number[]) {
-    this.productsArray = products;
-  }
-
-  //Metodo che aggiunge un prodotto al carrello
-  addToCart(prodotto : number) {
-    this.productsArray.push(prodotto);
-    console.log("aggiunto un prodotto");
-  }
-
-  //Metodo che rimuove dall'array prodotti ciò che si vuole togliere
-  removeFromCart(prodotto : number) {
-    this.productsArray.splice(this.productsArray.indexOf(prodotto), 1);
-  }
-
-  //Ritorna l'array di prodotti all'interno del carrello
-  getProducts() : number[] {
-    return this.productsArray;
-  }
-
-}
-
 
 @Component({
   selector: 'app-cart',
@@ -34,22 +8,28 @@ export class ShoppingCart {
 })
 export class CartComponent implements OnInit {
 
-  constructor() { 
-    this.cont += 1;
-    this.shoppingCart.addToCart(this.cont)
-    console.log(this.shoppingCart.getProducts());
-    console.log("entrato nel costruttore di CartComponent");
+  //Ad ogni caricamento della pagina va ad aggiungere all'array products i prodotti effettivamente all'interno della classe ShoppingCart
+  constructor(private cartService : ShoppingCartService) {
+    this.cartService.getCartInstance().addToCart(Math.random());
+    this.products = this.cartService.getCartInstance().getProducts();
+    console.log(this.products); //debug
   }
 
   //Inizializza oggetto carrello e suoi prodotti
-  ngOnInit(): void {
-    console.log("NG ON INIT")
+  ngOnInit(): void {}
+
+  products : number[];
+
+  deleteProduct(product : number) {
+
+    this.cartService.getCartInstance().removeFromCart(product);
+
   }
 
-  shoppingCart = new ShoppingCart([]);
-  products : number[] = this.shoppingCart.getProducts();
-  cont = 0;
+  addProduct() {
 
+    this.cartService.getCartInstance().addToCart(Math.random());
 
+  }
 
 }
